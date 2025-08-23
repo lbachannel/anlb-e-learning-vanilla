3af66b7290000f7667e7a4d4b16ef9880d25cd82
lbachannel <anlb19749@gmail.com>
2025-08-23 14:10:57 +0700

    layout #1

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,3 @@ import RenderLayout from "./components/layout/render.js";
 const app = document.querySelector('#app');
 app.appendChild(RenderLayout());
 
-// SPA
-const router = new Router('#app', {
-    '/': { title: 'Home', render: home },
-    '/about': { title: 'About', render: about },
-    '/contact': { title: 'Contact', render: contact }
-});
-
-// router.init();
-
