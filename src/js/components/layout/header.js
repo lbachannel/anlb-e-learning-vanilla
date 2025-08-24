39184771133d6738801139dd84d484e49259687a
lbachannel <anlb19749@gmail.com>
2025-08-24 12:06:43 +0700

    header search

--- src/js/components/layout/header.js
+++ src/js/components/layout/header.js
@@ -14,13 +14,59 @@ const _loadLogo = () => {
     return div;
 }
 
-const _loadNavBar = () => {
+const _clearInput = (input, clearBtn) => {
+    input.value = '';
+    clearBtn.style.display = 'none';
+    input.focus();
+}
+
+const _loadSearch = () => {
     const div = document.createElement('div');
-    div.className = 'navbar';
+    div.className = 'header__search';
+
+    const form = document.createElement('form');
+    form.className = 'header__form';
+    const icon = document.createElement('img');
+    icon.src = 'search.svg';
+    icon.className = 'header__input-icon'
+    const input = document.createElement('input');
+    input.placeholder = 'Search here...';
+    input.className = 'header__input';
+    input.name = '';
+
+    const clearBtn = document.createElement('span');
+    clearBtn.className = 'header__input-clear-btn';
+    clearBtn.innerText = '✖';
+    clearBtn.style.display = 'none';
+
+    form.append(icon, input, clearBtn);
+    div.append(form);
+
+    input.oninput = (event) => {
+        if (event.target.value) {
+            clearBtn.style.display = 'inline';
+        } else {
+            clearBtn.style.display = 'none';
+        }
+    }
+
+    clearBtn.onclick = () => {
+        _clearInput(input, clearBtn);
+    }
+
+    return div;
+}
+
+const _loadHeaderMenu = () => {
+    const div = document.createElement('div');
+    div.className = 'header__menu';
 
     // search
-
+    const search = _loadSearch();
     // settings
+
+
+    div.append(search);
     return div;
 }
 
@@ -30,9 +76,9 @@ const Header = () => {
 
     // logo + nav
     const headerLogo = _loadLogo();
-    const headerNav = _loadNavBar();
+    const headerMenu = _loadHeaderMenu();
 
-    header.append(headerLogo, headerNav);
+    header.append(headerLogo, headerMenu);
 
     return header;
 }
