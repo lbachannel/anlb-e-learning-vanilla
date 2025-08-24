d58e1d0dd4a87a49faacb1519de6fa3fff9a1524
lbachannel <anlb19749@gmail.com>
2025-08-24 09:45:18 +0700

    header logo

--- src/js/components/layout/header.js
+++ src/js/components/layout/header.js
@@ -1,8 +1,40 @@
+const _loadLogo = () => {
+    const div = document.createElement('div');
+    div.className = 'logo';
+
+    const logo = document.createElement('img');
+    logo.src = 'logo.svg';
+    logo.className = 'logo__icon';
+
+    const text = document.createElement('p');
+    text.className = 'logo__text';
+    text.innerText = 'Learning English';
+
+    div.append(logo, text);
+    return div;
+}
+
+const _loadNavBar = () => {
+    const div = document.createElement('div');
+    div.className = 'navbar';
+
+    // search
+
+    // settings
+    return div;
+}
+
 const Header = () => {
     const header = document.createElement('header');
-    header.innerHTML = 'This is header';
-    header.style.background = 'yellow';
-    return header
+    header.className = 'header';
+
+    // logo + nav
+    const headerLogo = _loadLogo();
+    const headerNav = _loadNavBar();
+
+    header.append(headerLogo, headerNav);
+
+    return header;
 }
 
 export default Header;
